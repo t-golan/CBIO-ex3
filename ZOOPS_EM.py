d11d653b997f79bd70e36b0abc26a081b8da8664
rotem_gil <rotem,gil@mail.huji.ac.il>
2021-12-12 14:56:08 +0200

    added initial emissions

--- ZOOPS_EM.py
+++ ZOOPS_EM.py
@@ -1,5 +1,14 @@
 import argparse
-# from motif_find import...
+from Bio import SeqIO
+import pandas as pd
+import numpy as np
+
+
+def get_seq(fasta):
+    seq_lst = []
+    for f in fasta:
+        seq_lst.append("^" + str(f.seq) + "$")
+    return seq_lst
 
 
 def parse_args():
@@ -18,14 +27,44 @@ def parse_args():
     return parser.parse_args()
 
 
+def initial_emissions(alpha, motif):
+    # initalize emissions dataframe for motif, filled with alpha
+    k = len(motif)
+    df = pd.DataFrame(
+        {"A": np.full(k, alpha),
+         "C": np.full(k, alpha),
+         "G": np.full(k, alpha),
+         "T": np.full(k, alpha)})
+    # for each state in motif update emission to 1-3*alpha
+    for i in range(k):
+        df[motif[i]][i] = 1 - (3*alpha)
+    df['^'] = np.zeros(df.shape[0])
+    df['$'] = np.zeros(df.shape[0])
+    # add emissions for states not in motif
+    rows = pd.DataFrame(np.zeros((4, df.shape[1])), columns=df.columns)
+    df = pd.concat([rows, df], ignore_index=True)
+    # 3 represents 'Bend', which emits '$' with probability 1
+    df['$'][3] = 1
+    # 0 represents 'Bstart', which emits '^' with probability 1
+    df['^'][0] = 1
+    # 1 and 2 represent B1 and B2, which emit AGCT with probability 0.25
+    df.iloc[1:3, 0:4] = 0.25
+    # return log
+    with np.errstate(divide='ignore'):
+        return df.apply(np.log)
+
+
 def main():
     args = parse_args()
 
     # build transitions 
 
-    # build emissions 
+    # build emissions
+    initial_ems = initial_emissions(args.alpha, args.seed)
 
     # load fasta
+    seq_lst = get_seq(SeqIO.parse(open(args.fasta), 'fasta'))
+    print(seq_lst)
 
     # run Baum-Welch
 
